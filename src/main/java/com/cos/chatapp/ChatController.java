11220633c39542446d78bef637c7c30f057b300c
dt1 <suuuu3030@gmail.com>
2025-03-04 15:56:19 +0900

    feat : 방 번호로 채팅할 수 있는 기능 추가

--- src/main/java/com/cos/chatapp/ChatController.java
+++ src/main/java/com/cos/chatapp/ChatController.java
@@ -17,12 +17,21 @@ public class ChatController {
 
     private final ChatRepository chatRepository;
 
+    // 귓속말 시에만 사용
     @CrossOrigin
     @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<Chat> getMsg(@PathVariable String sender, @PathVariable String receiver) {
         return chatRepository .mfindBySender(sender, receiver)
                 .subscribeOn(Schedulers.boundedElastic());
     }
+    @CrossOrigin
+    @GetMapping(value = "/chat/roomNum/{roomNum}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> findByRoomNum(@PathVariable Integer roomNum) {
+        return chatRepository .mfindByRoomNum(roomNum)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+
     @CrossOrigin
     @PostMapping("/chat")
     public Mono<Chat> setMsg(@RequestBody Chat chat) {//데이터 한건만 리턴할거면 mono, 아니면 flux. save라서 리턴 안해도되는데 리턴 데이터 보려고 mono로 설정한 것.
--- src/main/java/com/cos/chatapp/ChatRepository.java
+++ src/main/java/com/cos/chatapp/ChatRepository.java
@@ -11,4 +11,8 @@ public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
     @Query("{sender: ?0, receiver: ?1}")
     Flux<Chat> mfindBySender(String sender, String receiver); //Flux(흐름) response를 유지하면서 데이터를 계속 흘려보내기
 
+    @Tailable //커서를 닫지 않고 계속 유지한다.
+    @Query("{roomNum: ?0}")
+    Flux<Chat> mfindByRoomNum(Integer roomNum);
+
 }
